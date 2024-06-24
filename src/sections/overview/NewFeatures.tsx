c4a2ca68e142c63a99b29d7d7fc21d6238826c8d
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-06-24 12:22:53 -0400

    added release selector

--- src/sections/overview/NewFeatures.tsx
+++ src/sections/overview/NewFeatures.tsx
@@ -1,36 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
+import { Dropdown } from '@/components/Dropdown';
 
 type Release = {
+  id: number;
   name: string;
   body: string;
   html_url: string;
 };
 
 export const NewFeatures = () => {
-  const [releaseNotes, setReleaseNotes] = useState<Release | null>(null);
+  const [releases, setReleases] = useState<Release[]>([]);
+  const [selectedRelease, setSelectedRelease] = useState<Release | null>(null);
 
   useEffect(() => {
-    const fetchLatestRelease = async () => {
+    const fetchReleases = async () => {
       const response = await fetch(
-        'https://api.github.com/repos/praetorian-inc/chariot-ui/releases/latest'
+        'https://api.github.com/repos/praetorian-inc/chariot-ui/releases'
       );
-      const data: Release = await response.json();
-      setReleaseNotes(data);
+      const data: Release[] = await response.json();
+      setReleases(data);
+      setSelectedRelease(data[0]);
     };
 
-    fetchLatestRelease();
+    fetchReleases();
   }, []);
 
-  if (!releaseNotes) {
+  if (!selectedRelease) {
     return <div>Loading...</div>;
   }
 
-  const { name, body, html_url } = releaseNotes;
+  const { name, body, html_url } = selectedRelease;
+
+  const isLatest = selectedRelease.id === releases[0].id;
 
   return (
     <div className="rounded-[2px] bg-white p-6 shadow-md">
-      <h2 className="mb-4 text-2xl font-bold">{name}</h2>
+      <div className="border-1 mb-6 flex items-center justify-between">
+        <Dropdown
+          label={name}
+          className="font-semibold"
+          endIcon={<ChevronDownIcon className="size-3 stroke-[4px]" />}
+          menu={{
+            items: releases.map(release => ({
+              label: release.name,
+              value: release.name,
+            })),
+            onClick: value => {
+              const release = releases.find(r => r.name === value);
+              setSelectedRelease(release || null);
+            },
+            value: name,
+          }}
+        />
+        <span className="text-sm italic">
+          {isLatest ? 'Current Release' : 'Previous Release'}
+        </span>
+      </div>
       <div className="list-inside list-disc text-gray-700">
         <Markdown className="prose">{body}</Markdown>
         <a href={html_url} className="text-primary">

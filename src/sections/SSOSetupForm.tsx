9bdaf95d59b0d7cb1943fb1338ad7f1cd94d2ced
harshita2405 <harshita.singh@praetorian.com>
2024-07-12 11:53:03 +0530

    Updates Inputs

--- src/sections/SSOSetupForm.tsx
+++ src/sections/SSOSetupForm.tsx
@@ -1,34 +1,36 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 
 import { Button } from '@/components/Button';
+import { Chip } from '@/components/Chip';
+import { Input } from '@/components/form/Input';
+import { Inputs } from '@/components/form/Inputs';
 import { Modal } from '@/components/Modal';
 import { useMy } from '@/hooks';
 import { useModifyAccount } from '@/hooks/useAccounts';
 
+const DefaultFormValues = {
+  domain: '',
+  clientId: '',
+  secret: '',
+  issuerUrl: '',
+};
+
 export const SSOSetupForm = () => {
-  const { data: accounts, refetch } = useMy({ resource: 'account' });
-  const { mutate: link, status } = useModifyAccount('link');
+  const { data: accounts } = useMy({ resource: 'account' });
+  const { mutate: link } = useModifyAccount('link');
   const { mutate: unlink } = useModifyAccount('unlink');
 
   const [showModal, setShowModal] = useState(false);
-  const [domain, setDomain] = useState('');
-  const [clientId, setClientId] = useState('');
-  const [secret, setSecret] = useState('');
-  const [issuerUrl, setIssuerUrl] = useState('');
+  const [formData, setFormData] = useState(DefaultFormValues);
 
   const ssoAccount = useMemo(
     () => accounts?.find(account => account.member.startsWith('sso:')),
     [accounts]
   );
 
-  useEffect(() => {
-    if (status === 'success') {
-      refetch();
-    }
-  }, [status]);
-
   const handleFormSubmit = () => {
+    const { domain, clientId, secret, issuerUrl } = formData;
     link({
       username: `sso:${domain}`,
       value: `sso:${domain}`,
@@ -41,13 +43,16 @@ export const SSOSetupForm = () => {
     });
     // Close the modal after submission
     setShowModal(false);
+    setFormData(DefaultFormValues);
   };
 
   return (
     <>
       {ssoAccount ? (
-        <div className="flex flex-row space-x-6">
-          <span>{ssoAccount.member.split(':')[1]}</span>
+        <div className="mt-2 flex flex-row space-x-6">
+          <Chip className="text-md px-6" style="primary">
+            {ssoAccount.member.split(':')[1]}
+          </Chip>
           <button
             className="jusify-center flex flex-row items-center space-x-2 text-sm font-medium"
             onClick={() =>
@@ -67,67 +72,56 @@ export const SSOSetupForm = () => {
         title="Setup SSO"
         size="md"
         footer={{
+          form: 'sso-setup-form',
           text: 'Save',
-          onClick: handleFormSubmit,
         }}
       >
-        <form className="flex flex-col space-y-4 p-4">
-          <p className="mb-6 ">
+        <form
+          className="flex flex-col space-y-4"
+          id="sso-setup-form"
+          onSubmit={handleFormSubmit}
+        >
+          <p className="text-sm font-bold">
             Add a TXT record to your domain with the following value:{' '}
             <code>chariot=&lt;email&gt;</code>
           </p>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Domain
-            </label>
-            <input
-              type="text"
-              value={domain}
-              onChange={e => setDomain(e.target.value)}
-              className="mt-1 block w-full  border-gray-300 p-2 shadow-sm"
-              required
-              placeholder="acme.com"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Client ID
-            </label>
-            <input
-              type="text"
-              value={clientId}
-              onChange={e => setClientId(e.target.value)}
-              className="mt-1 block w-full  border-gray-300 p-2 shadow-sm"
-              required
-              placeholder="1a2b3c4d-5e6f-7g8h-9i0j"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Secret
-            </label>
-            <input
-              type="password"
-              value={secret}
-              onChange={e => setSecret(e.target.value)}
-              className="mt-1 block w-full  border-gray-300 p-2 shadow-sm"
-              required
-              placeholder="**********"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Issuer URL
-            </label>
-            <input
-              type="text"
-              value={issuerUrl}
-              onChange={e => setIssuerUrl(e.target.value)}
-              className="mt-1 block w-full  border-gray-300 p-2 shadow-sm"
-              required
-              placeholder="https://login.microsoftonline.com/1a2b3c4d-5e6f-7g8h-9i0j/v2.0"
-            />
-          </div>
+          <Inputs
+            inputs={[
+              {
+                label: 'Domain',
+                value: formData.domain,
+                placeholder: 'acme.com',
+                name: 'domain',
+                required: true,
+              },
+              {
+                label: 'Client ID',
+                value: formData.clientId,
+                placeholder: '1a2b3c4d-5e6f-7g8h-9i0j',
+                name: 'clientId',
+                required: true,
+              },
+              {
+                label: 'Secret',
+                value: formData.secret,
+                type: Input.Type.PASSWORD,
+                placeholder: '**********',
+                name: 'secret',
+                required: true,
+              },
+              {
+                label: 'Issuer URL',
+                value: formData.issuerUrl,
+                placeholder:
+                  'https://login.microsoftonline.com/1a2b3c4d-5e6f-7g8h-9i0j/v2.0',
+                name: 'issuerUrl',
+                required: true,
+              },
+            ]}
+            onChange={values =>
+              setFormData(formData => ({ ...formData, ...values }))
+            }
+          />
         </form>
       </Modal>
     </>

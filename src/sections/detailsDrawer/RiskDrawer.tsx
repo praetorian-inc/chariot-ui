1be4a4ec5b4774d9cbbc35f3a02c05436b3e4088
Subbiah Chandramouli <subbiah.chandramouli@praetorian.com>
2024-07-12 01:01:19 -0500

    Sanitize MarkdownPreview text

--- src/sections/detailsDrawer/RiskDrawer.tsx
+++ src/sections/detailsDrawer/RiskDrawer.tsx
@@ -9,7 +9,6 @@ import {
 } from '@heroicons/react/24/outline';
 import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import { TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
-import DOMPurify from 'dompurify';
 
 import { Button } from '@/components/Button';
 import { Drawer } from '@/components/Drawer';
@@ -360,7 +359,7 @@ export function RiskDrawer({ compositeKey, open }: RiskDrawerProps) {
                     }}
                   >
                     <MarkdownEditor
-                      value={DOMPurify.sanitize(markdownValue)}
+                      value={markdownValue}
                       onChange={value => {
                         setMarkdownValue(value || '');
                       }}
@@ -370,7 +369,7 @@ export function RiskDrawer({ compositeKey, open }: RiskDrawerProps) {
                   <>
                     {definitionsFile && (
                       <MarkdownPreview
-                        source={DOMPurify.sanitize(definitionsFileValue)}
+                        source={definitionsFileValue}
                         style={{
                           wordBreak: 'break-word',
                           minHeight: '20px',
